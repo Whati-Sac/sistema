0877e4fb9f1410f54d9933b590d45cf05f7c84fa
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2024-09-04 00:53:52 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,7 @@ const App = () => {
                 total: mode === "light" ? "#fff" : "#222",
                 messageIcons: mode === "light" ? "grey" : "#F3F3F3",
                 inputBackground: mode === "light" ? "#FFFFFF" : "#333",
-                barraSuperior: mode === "light" ? "linear-gradient(to right, #2DDD7F, #2DDD7F , #2DDD7F)" : "#666",
+                barraSuperior: mode === "light" ? "linear-gradient(to right, #0050ff, #0050ff , #0050ff)" : "#666",
 				boxticket: mode === "light" ? "#EEE" : "#666",
 				campaigntab: mode === "light" ? "#ededed" : "#666",
 				mediainput: mode === "light" ? "#ededed" : "#1c1c1c",
