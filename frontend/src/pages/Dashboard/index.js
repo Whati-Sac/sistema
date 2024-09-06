ac1daaa44db91e21ea3ff1c6af744577ceb91acd
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2024-09-06 15:35:17 -0300

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import BusinessCenterTwoToneIcon from '@material-ui/icons/BusinessCenterTwoTone';
-import businessIcon from '../assets/icons/business.json';
+import businessIcon from '../assets/icons/icon1.json';
 
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
@@ -476,7 +476,7 @@ const Dashboard = () => {
                 <Grid item xs={2}>
                   {/* Usando ícone local */}
                   <lord-icon
-                    src={businessIcon}  // Usando o caminho importado
+                    src={icon1}  // Usando o caminho importado
                     trigger="loop"
                     style={{ width: "100px", height: "100px", color: "#FFF" }}
                   />
